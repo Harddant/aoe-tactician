315f1a74b0e3890f8807b848065ce717ff638859
Harddant <rt.dekker@outlook.com>
2024-12-03 22:22:56 +0100

    Deleted unused space

--- src/app/components/CivPicker.tsx
+++ src/app/components/CivPicker.tsx
@@ -1,17 +1,10 @@
 "use client"
 
-
 import { api } from "@/trpc/react";
 import { type Civilization } from "@prisma/client";
 
-
-
-
 export const CivPicker = () => {
     const civilizationQuery = api.civilization.getAll.useQuery();
-
-
-
 
     return (
         <div>
@@ -21,57 +14,3 @@ export const CivPicker = () => {
         </div>
     );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use client";
-// import React, {useEffect, useState} from "react";
-//
-// export const CivPicker = () => {
-//
-//     const [name, setName] = useState("Select your Civilization");
-//
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const response = await fetch("");
-//                 const data = await response.json();
-//                 setName(data.name);
-//             } catch (error) {
-//                 console.error("Error fetching data:", error);
-//                 setName("Error fetching name");
-//             }
-//         }
-//         fetchData();
-//     }, [setName]);
-//
-//
-//     return (
-//         <div className="flex justify-center text-center my-6 text-[#c2ab73]">
-//             <h1 className="text-3xl font-bold">
-//                 {name}
-//             </h1>
-//         </div>
-//     );
-// };
