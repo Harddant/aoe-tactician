0dcaa067ff0e02bd805cebee6bdb88eb1cd0b563
Harddant <rt.dekker@outlook.com>
2025-01-29 23:24:58 +0100

    working on the guide component

--- src/app/components/GuideCiv.tsx
+++ src/app/components/GuideCiv.tsx
@@ -1,5 +1,20 @@
+"use client"
 
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { type Civilization } from "@prisma/client";
+import {api} from "@/trpc/react";
 
-export const GuideCiv = {
-
+export const GuideCiv = ({
+  selectedItem,
+  setSelectedItem,
+}: {
+  selectedItem: Civilization
+  setSelectedItem: (newItem: Civilization | null) => void
+}) => {
+  const civDetails = api.civilization.getCivilizationDetails.useQuery({civilizationId: selectedItem.id});
+  console.log("civ: ", civDetails?.data);
+  return(
+    <div></div>
+  )
 }
--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -3,8 +3,10 @@
 import { Navbar } from "@/app/components/Navbar";
 import { CivilizationSection } from "@/app/components/CivilizationSection";
 import { CounterCiv } from "@/app/components/CounterCiv";
+import { GuideCiv } from "@/app/components/GuideCiv";
 import { useState } from "react";
 import { type Civilization } from "@prisma/client";
+
 
 export const Wrapper = () => {
     const [selectedItem, setSelectedItem] = useState<Civilization | null>(null);
@@ -18,6 +20,7 @@ export const Wrapper = () => {
             {selectedItem && (
                 <section id="civ-counters" className="h-screen w-full">
                     <CounterCiv selectedItem={selectedItem} setSelectedItem={setSelectedItem}/>
+                    <GuideCiv selectedItem={selectedItem} setSelectedItem={setSelectedItem}/> 
                 </section>
             )}
         </div>
